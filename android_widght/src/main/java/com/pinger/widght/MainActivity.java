5439128608caa6bb63da5e75133f3d8f2248c922
PingerOne <wp520it@163.com>
2017-03-25 21:46:26 +0800

    添加控件module

--- android_widght/src/main/java/com/pinger/widght/MainActivity.java
+++ android_widght/src/main/java/com/pinger/widght/MainActivity.java
@@ -1,9 +1,7 @@
-package com.pinger.transition;
+package com.pinger.widght;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.transition.Transition;
-import android.transition.TransitionManager;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,8 +9,5 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-
     }
 }
